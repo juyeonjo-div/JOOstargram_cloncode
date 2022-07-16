55261120e3985729139b701b78586843ff449461
jonedoe code <95915443+daturainoxia@users.noreply.github.com>
2022-07-16 18:50:16 +0900

    Initial commit

---
+++ src/main/java/com/example/joostargram_cloncode/JoOstargramCloncodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.joostargram_cloncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoOstargramCloncodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoOstargramCloncodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joostargram_cloncode/JoOstargramCloncodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joostargram_cloncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoOstargramCloncodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
